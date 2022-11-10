45601dc0cf2fe48646f0c7ef43231adabd8c9523
AVS1508 <aditya.11.present@gmail.com>
2022-11-10 11:40:14 -0500

    Added markup and styles for additional pages

---
+++ Session 5/portfolio-website-2/src/pages/Projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import styles from '../page.module.css';
+
+const Projects = () => {
+  return (
+    <Container fluid className={styles.container}>
+      <h1 className={styles.heading}>Projects</h1>
+    </Container>
+  );
+};
+
+export default Projects;
